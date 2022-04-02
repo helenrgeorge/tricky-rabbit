e5e805c6f11226cbb85ca5f341d6d6737e7f2db5
helenrgeorge <39845977+helenrgeorge@users.noreply.github.com>
2022-04-02 18:11:37 -0500

    Update Project.js

--- Project.js
+++ Project.js
@@ -5,20 +5,41 @@ const thirButton = document.getElementById("thirButton")
 
 let userOnescore = 0
 let userTwoscore = 0
+
 const H1 = document.getElementById("buttons")
 
 H1.innerText = userOnescore + " to " + userTwoscore
 
+
+let numb = document.getElementById("list")
+
+
+numb.addEventListener("click", function (e) {
+    
+    let MaxScore = e.target.innerHTML
+
+
+})
+
 firButton.addEventListener("click", function() {
     userOnescore = userOnescore + 1
     H1.innerText = userOnescore + " to " + userTwoscore
+    if (userOnescore >= MaxScore) { 
+       /* let userTwoscore = userTwoscore.style.color = "red"
+        let userOnescore = userOnescore.style.color = "green"
+        alert("User One Wins!") */
+
+        console.log("winner")
+    }
 })
 
 secButton.addEventListener("click", function() {
     userTwoscore = userTwoscore + 1
     H1.innerText = userOnescore + " to " + userTwoscore
-    if (userTwoscore > 4) { 
-        let userTwoscore = userTwoscore.style.color = "green"
+    if (userTwoscore >= MaxScore) { 
+        userTwoscore.style.color = "green"
+        userOnescore.style.color = "red"
+        alert("User Two Wins!")
     }
     })
 
